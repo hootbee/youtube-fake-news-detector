b3adcf2d6c3fac61ab310f303d31d38bc0a76454
hootbee <xx3xx27@gmail.com>
2025-05-06 02:16:29 +0900

    주석 삭제

--- node-server/src/api/routes/analysis.js
+++ node-server/src/api/routes/analysis.js
@@ -11,22 +11,6 @@ router.post("/text", (req, res) => {
   analysisController.receiveTextData(req, res);
 });
 
-// // 오디오 다운로드
-// router.post("/download", (req, res) => {
-//   res.header("Access-Control-Allow-Methods", "POST");
-//   res.header("Access-Control-Allow-Headers", "Content-Type");
-
-//   analysisController.downloadAudio(req, res);
-// });
-
-// // 자막 요약/보정
-// router.post("/summarize", (req, res) => {
-//   res.header("Access-Control-Allow-Methods", "POST");
-//   res.header("Access-Control-Allow-Headers", "Content-Type");
-
-//   analysisController.summarizeCaptions(req, res);
-// });
-
 router.post("/analyze", (req, res) => {
   res.header("Access-Control-Allow-Methods", "POST");
   res.header("Access-Control-Allow-Headers", "Content-Type");
